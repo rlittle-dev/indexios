057744c57691f54f96fc8d1370b1b02ba7168a8d
Ryan <rjlittle25@gmail.com>
2026-01-14 21:07:36 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -5,12 +5,12 @@ import Home from './pages/Home';
 import ManageDevices from './pages/ManageDevices';
 import MyAccount from './pages/MyAccount';
 import Pricing from './pages/Pricing';
+import SavedCandidates from './pages/SavedCandidates';
+import Scan from './pages/Scan';
 import SharedReport from './pages/SharedReport';
 import Team from './pages/Team';
 import TeamDashboard from './pages/TeamDashboard';
 import Tickets from './pages/Tickets';
-import SavedCandidates from './pages/SavedCandidates';
-import Scan from './pages/Scan';
 import __Layout from './Layout.jsx';
 
 
@@ -22,12 +22,12 @@ export const PAGES = {
     "ManageDevices": ManageDevices,
     "MyAccount": MyAccount,
     "Pricing": Pricing,
+    "SavedCandidates": SavedCandidates,
+    "Scan": Scan,
     "SharedReport": SharedReport,
     "Team": Team,
     "TeamDashboard": TeamDashboard,
     "Tickets": Tickets,
-    "SavedCandidates": SavedCandidates,
-    "Scan": Scan,
 }
 
 export const pagesConfig = {
